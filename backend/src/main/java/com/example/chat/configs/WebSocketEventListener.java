9c7bc36e1c2303b0f22568c519b6afdaf2138fee
Tawan1101 <wanchailungnoeng2019@gmail.com>
2025-02-11 23:25:30 +0700

    update1

--- backend/src/main/java/com/example/chat/configs/WebSocketEventListener.java
+++ backend/src/main/java/com/example/chat/configs/WebSocketEventListener.java
@@ -1,27 +1,35 @@
-package com.example.chat.configs;
-
-import com.example.chat.models.ChatMessage;
-import com.example.chat.models.MessageType;
-import lombok.AllArgsConstructor;
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 @Component
-@AllArgsConstructor
-public class WebSocketEventListener {
-    private final SimpMessageSendingOperations messagingTemplate;
+public class WebSocketEventListener extends TextWebSocketHandler {
+    private static final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        broadcastOnlineUsers();
+    }
 
-    @EventListener
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
-        if(username  != null) {
-            ChatMessage chatMessage = ChatMessage.buildChatmessage(username + "has left the chat." , username , MessageType.LEAVE);
-            messagingTemplate.convertAndSend("/topic/messages" , chatMessage);
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        broadcastOnlineUsers();
+    }
+
+    private void broadcastOnlineUsers() {
+        int onlineUsers = sessions.size();
+        String message = "{\"type\": \"ONLINE_USERS\", \"onlineUsers\": " + onlineUsers + "}";
+
+        for (WebSocketSession session : sessions) {
+            try {
+                session.sendMessage(new TextMessage(message));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
